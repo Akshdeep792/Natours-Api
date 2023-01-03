9664a1371c5a33ee00003d5e8d27c4c128116055
Akshdeep Singh <76034766+Akshdeep792@users.noreply.github.com>
2023-01-03 11:23:28 +0530

    Embedding Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -100,7 +100,8 @@ const tourSchema = new mongoose.Schema({
             description: String,
             day: Number
         }
-    ]
+    ],
+    guides: Array
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -111,11 +112,18 @@ tourSchema.virtual('durationWeeks').get(function () {
 })
 
 //document middleware
-// tourSchema.pre('save', function (next) {
-//     console.log(this)
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true })
+
+    next()
+})
+
+// embedding tours
+// tourSchema.pre('save', async function (next) {
+//     const guidesPromises = this.guides.map(async id => await URLSearchParams.findById(id));
+//     this.guides = await Promise.all(guidesPromises)
 //     next()
 // })
-
 // tourSchema.post('save', function (doc, next) {
 //     console.log(doc)
 //     next()
