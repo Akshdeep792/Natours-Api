a4c833a2f20a39d033350d6329cb14b7387945ef
Akshdeep Singh <76034766+Akshdeep792@users.noreply.github.com>
2023-01-13 14:18:39 +0530

    Ratings Average Function

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Tour = require('./tourModel')
 const reviewSchema = new mongoose.Schema({
     review: {
         type: String,
@@ -46,5 +47,51 @@ reviewSchema.pre(/^find/, function (next) {
     next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId },
+
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ])
+    console.log(stats)
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating
+        })
+    }
+    else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 0
+        })
+    }
+}
+reviewSchema.post('save', function () {
+    this.constructor.calcAverageRatings(this.tour)
+
+})
+
+//update and delete review
+//query middleware
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    this.r = await this.findOne();
+    console.log(this.r)
+    next()
+})
+
+reviewSchema.post(/^findOneAnd/, async function () {
+
+    // this.r = await this.findOne(); this will not work here as query already has been executed
+    await this.r.constructor.calcAverageRatings(this.r.tour)
+})
 const Review = mongoose.model('Review', reviewSchema)
 module.exports = Review
