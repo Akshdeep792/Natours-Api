ef0f820675a31903f28190b530812a6ce839af9d
Akshdeep Singh <76034766+Akshdeep792@users.noreply.github.com>
2023-01-14 15:59:17 +0530

    Server side rendering

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 const express = require("express")
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
@@ -12,6 +13,11 @@ const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+//serving static files
+app.use(express.static(path.join(__dirname, 'public'))) // to server static files
 //middlewares
 app.use(helmet())//set security headers
 
@@ -46,14 +52,17 @@ app.use(hpp({
     ]
 })) // parameter polution handeling
 
-//serving static files
-app.use(express.static(`${__dirname}/public`)) // to server static files
 // app.use((req, res, next) => {
 //     console.log("Hello from the middleware 👋")
 //     next()
 // })
 
-
+app.get('/', (req, res) => {
+    res.status(200).render('base', {
+        tour: 'The Forest Hiker',
+        user: 'Akshdeep Singh'
+    })
+})
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/reviews', reviewRouter)
